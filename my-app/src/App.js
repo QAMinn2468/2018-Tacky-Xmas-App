5fa020c874462841f9b07fd0d80373a3bbed278b
Kimberly McCaffrey <Kimberly.M.McCaffrey@gmail.com>
2018-12-04 22:13:46 -0500

    Continuing to transition to React

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Banner from './Banner.js'
+// import Sidebar from './sidebar.js'
+// <Sidebar />
+
+
+/*********************************Start of Component***************************/
 
 class App extends Component {
+  // Insert state here
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <Banner />
+
       </div>
     );
   }
---
+++ my-app/src/Banner.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+/*********************************Start of Component***************************/
+
+const Banner = ({name}) => {
+
+    return (
+      <div className="banner">MAXX Potential Christmas Party 2018</div>
+    )
+  }
+
+export default Banner
